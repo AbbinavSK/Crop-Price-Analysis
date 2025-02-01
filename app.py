07cd62e4acaf7e7d40598d92475cfe0c9ed62231
Abbinav Sankar Kailasam <abbinavsankar2003@gmail.com>
2025-02-01 15:17:35 +0530

    Update app.py

--- app.py
+++ app.py
@@ -97,9 +97,9 @@ district = st.sidebar.selectbox('Districts', mp_districts)
 
 #--------------------------------------------------------------------------------------------------------------------------------------
 # Importing and cleaning the data
-data = pd.read_excel("data\SOYBEAN-MODALPRICE-MONTHLY(Selected).xlsx")
-prec_data = pd.read_excel("data\PRECTOTCORR_MONTHLY.xlsx", sheet_name="Madhya Pradesh")
-tmax_data = pd.read_excel("data\T2M_MAX_MONTHLY.xlsx", sheet_name="Madhya Pradesh")
+data = pd.read_excel("data/SOYBEAN-MODALPRICE-MONTHLY(Selected).xlsx")
+prec_data = pd.read_excel("data/PRECTOTCORR_MONTHLY.xlsx", sheet_name="Madhya Pradesh")
+tmax_data = pd.read_excel("data/T2M_MAX_MONTHLY.xlsx", sheet_name="Madhya Pradesh")
 
 data["Price Date"] = pd.to_datetime(data["Price Date"])
 prec_data = cleaning_met_data(prec_data)
@@ -109,7 +109,7 @@ df_logreturns = price_log_returns(data)
 df_condvol = price_cond_vol(df_logreturns)
 
 # Importing and cleaning the LSTM prediction data
-lstm_data = pd.read_csv("data\Soybean-MP-districtlevel-LSTMpred.csv")
+lstm_data = pd.read_csv("data/Soybean-MP-districtlevel-LSTMpred.csv")
 lstm_dates = df_condvol["Price Date"].iloc[-len(lstm_data):].reset_index(drop=True)
 df_lstm_pred = pd.DataFrame({
     "Price Date": lstm_dates,
