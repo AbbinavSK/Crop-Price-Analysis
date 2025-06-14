9dc9bf3357e2983272b6d3e09ecdc2a2fb124adc
Abbinav Sankar Kailasam <abbinavsankar2003@gmail.com>
2025-06-14 18:52:12 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,9 +7,9 @@ import pandas as pd
 import numpy as np
 import plotly.graph_objects as go
 #--------------------------------------------------------------------------------------------------------------------------------------
-
+# Path to data file and default image
 default_image = "India_Map.jpeg"
-data_path = "Combined_data(State-level).csv"
+data_path = "data.csv"
 
 #--------------------------------------------------------------------------------------------------------------------------------------
 # Function to read the necessary csv files
@@ -58,11 +58,13 @@ def plot_graph(x_values, y_values, labels, colors, xaxis_title, yaxis_title, y_r
     st.plotly_chart(fig, use_container_width=True)
 
 #--------------------------------------------------------------------------------------------------------------------------------------
+# Reading the csv files
 data = read_csv(data_path)
 states = data["State"].unique()
-#--------------------------------------------------------------------------------------------------------------------------------------
 
-st.title("Assessing the Impact of Meteorological Factors on Crop Price Volatility in India: Case studies of Soybean in Madhya Pradesh and Brinjal in Odisha")
+#--------------------------------------------------------------------------------------------------------------------------------------
+# Main body of the Streamlit app
+st.title("Impact of Meteorological Factors on Crop Price Volatility in India: Case studies of Soybean and Brinjal")
 selected_state = st.sidebar.selectbox('Select a State', ['Select a State'] + list(states))
 
 try:
@@ -71,15 +73,26 @@ try:
         with tabs1:
             state_data = data[data["State"] == selected_state]
             state_data = data[data["State"] == selected_state].sort_values(by="Price Date")
+            crop = state_data["Crop"].unique()[0]
 
+            st.markdown(f"Modal Price of {crop} in {selected_state} from 2012 to 2024: ")
             plot_graph(x_values=[state_data["Price Date"]], y_values=[state_data["State_Mean"]], labels="Modal Price", colors=["cyan"], xaxis_title="Date", yaxis_title="Modal Price (Rs./Quintal)")
+
+            st.markdown(f"Log Returns of {crop} in {selected_state} from 2012 to 2024: ")
             plot_graph(x_values=[state_data["Price Date"]], y_values=[state_data["State_Mean_LogReturns"]], labels="Log Returns", colors=["green"], xaxis_title="Date", yaxis_title="Log Returns", y_range=[-0.81, 0.81])
+
+            st.markdown(f"Squared Log Returns of {crop} in {selected_state} from 2012 to 2024: ")
             plot_graph(x_values=[state_data["Price Date"]], y_values=[state_data["State_Mean_Squared_LogReturns"]], labels="Squared Log Returns", colors=["yellow"], xaxis_title="Date", yaxis_title="Squared Log Returns", y_range=[0, 0.63])
+
+            st.markdown(f"Conditional Volatility of {crop} in {selected_state} from 2012 to 2024: ")
             plot_graph(x_values=[state_data["Price Date"]], y_values=[state_data["State_Mean_LogReturns_CondVol"]], labels="Conditional Volatility", colors=["magenta"], xaxis_title="Date", yaxis_title="Conditional Volatility", y_range=[0, 0.7])
     
         with tabs2:
-            volsurface_path = f"{selected_state}_VolatilitySurface_2D_3D.gif"
-            st.image(volsurface_path, caption=f"2D and 3D Volatility Surfaces for {selected_state}", use_container_width=True)
+            volsurface_path_2D = f"{selected_state}_VolatilitySurface_2D.gif"
+            volsurface_path_3D = f"{selected_state}_VolatilitySurface_3D.gif"
+            st.image(volsurface_path_2D, caption=f"2D Plot for {selected_state}", output_format="GIF")
+            st.write("")
+            st.image(volsurface_path_3D, caption=f"3D Plot for {selected_state}", output_format="GIF")
     else:
         st.image(default_image, use_container_width=True)
 
